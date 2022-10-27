1eb4bc5ec81bef6d375336061b60605a654f97d6
koesnam <76582376+10kseok@users.noreply.github.com>
2022-10-28 02:12:44 +0900

    part5 How to test in Django

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from .models import Choice, Question
+from django.utils import timezone
 from django.template import loader
 from django.views import generic
+from .models import Choice, Question
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -11,10 +12,16 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        최근 발행된 질문 5개 반환 ( 미래가 포함되지 않은 )
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
+    # template_name default value : question_detail.html
     template_name: str = 'polls/detail.html'
 
 class ResultsView(generic.DetailView):
