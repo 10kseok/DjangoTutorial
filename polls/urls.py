5509b5249f9d5f317a3a3440a7e330785af915f8
koesnam <76582376+10kseok@users.noreply.github.com>
2022-10-27 16:10:30 +0900

    part3 How to connect Templates & Views / Relation with templates, urls and views

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
-
+    # ex: /polls/specifics/1/ , name이 detali.html에서 url 장고태그 써서 인식할 때 사용됨. url 바꾸려면 템플릿말고 여기서 바꾸면 됌.
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/1/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/1/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,33 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from .models import Question
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # # context는 템플릿에서 쓰이는 변수명과 Python 객체를 연결하는 사전형 값
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+
+    # 위 방법에서 단축된 형태
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context) # context는 optional
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
